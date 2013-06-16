1f3420e57c3bee38f4d0914afe00dbb48dfd2ba7
FrankZZ <frank@fwdev.nl>
2013-06-16 11:58:56 +0200

    Merged SSLServer into ControlServer

--- WebServer/Init.cs
+++ WebServer/Init.cs
@@ -4,9 +4,6 @@ namespace WebServer.Models
 {
 	public static class Init
 	{
-		private const string HOST = "0.0.0.0";
-		private const int PORT = 3333;
-
 		public static void Main(string[] args)
 		{
 			var config = new ConfigLoader("Config.xml").Read();
@@ -16,20 +13,13 @@ namespace WebServer.Models
 				Convert.ToInt32(config.GetEntry("Port"))
 			);
 
-			var sslServer = new SSLServer(
-				config.GetEntry("Host"),
-				443
-			);
-
 			var controlServer = new ControlServer(
 				config.GetEntry("Host"),
 				Convert.ToInt32(config.GetEntry("Port")) + 1
 			);
 
 			server.Run();
-			sslServer.Run();
 			controlServer.Run();
-
 
 		}
 	}
--- WebServer/Models/ControlServer.cs
+++ WebServer/Models/ControlServer.cs
@@ -2,16 +2,41 @@
 using System.Net.Sockets;
 using System.Net;
 using System.IO;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Authentication;
 
 namespace WebServer.Models
 {
 	public class ControlServer : Server
 	{
+		private X509Certificate certificate = new X509Certificate("Certificate\\Certificate.pfx", "KTYy77216");
+
 		override protected string WEBROOT
 		{
 			get { return Path.GetFullPath(Environment.CurrentDirectory + @"\ControlWebRoot"); }
 		}
 
-		public ControlServer(string host, int port): base(host, port) {}
+		public ControlServer(string host, int port) : base(host, port) { }
+
+		/* Override */
+		override protected void handleClient(NetworkStream ns, Socket so)
+		{
+			Console.WriteLine("[SSL] Handling incoming request");
+
+			try
+			{
+				SslStream sslStream = new SslStream(ns, false);
+
+				sslStream.AuthenticateAsServer(certificate, false, SslProtocols.Ssl3, false);
+
+				new ServerThread(sslStream, so, WEBROOT).Run();
+
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
 	}
 }
